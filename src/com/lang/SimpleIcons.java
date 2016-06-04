4519bb98ac52a0fa87aef29a983856f664cf45f1
Брызжин Иван <brizjin@gmail.com>
2016-06-04 15:35:58 +0300

    2.2. Define an icon

---
+++ src/com/lang/SimpleIcons.java
@@ -0,0 +1,9 @@
+package com.lang;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public class SimpleIcons {
+    public static final Icon FILE = IconLoader.getIcon("/com/simpleplugin/icons/stackoverflow-16x16.png");
+}
