40b5c223175b7a88199005ea77095588a26abc8e
Брызжин Иван <brizjin@gmail.com>
2016-06-04 15:36:28 +0300

    2.4. Define a file type factory

---
+++ src/com/lang/SimpleFileTypeFactory.java
@@ -0,0 +1,11 @@
+package com.lang;
+
+import com.intellij.openapi.fileTypes.*;
+import org.jetbrains.annotations.NotNull;
+
+public class SimpleFileTypeFactory extends FileTypeFactory {
+    @Override
+    public void createFileTypes(@NotNull FileTypeConsumer fileTypeConsumer) {
+        fileTypeConsumer.consume(SimpleFileType.INSTANCE, "simple");
+    }
+}
