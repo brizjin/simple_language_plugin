0587909802599ad36fa8a7d5f5f548fb2a292434
Брызжин Иван <brizjin@gmail.com>
2016-06-06 21:47:11 +0300

    3.4. Generate a parser

---
+++ gen/com/simpleplugin/psi/impl/SimplePropertyImpl.java
@@ -0,0 +1,29 @@
+// This is a generated file. Not intended for manual editing.
+package com.simpleplugin.psi.impl;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.util.PsiTreeUtil;
+import static com.simpleplugin.psi.SimpleTypes.*;
+import com.intellij.extapi.psi.ASTWrapperPsiElement;
+import com.simpleplugin.psi.*;
+
+public class SimplePropertyImpl extends ASTWrapperPsiElement implements SimpleProperty {
+
+  public SimplePropertyImpl(ASTNode node) {
+    super(node);
+  }
+
+  public void accept(@NotNull SimpleVisitor visitor) {
+    visitor.visitProperty(this);
+  }
+
+  public void accept(@NotNull PsiElementVisitor visitor) {
+    if (visitor instanceof SimpleVisitor) accept((SimpleVisitor)visitor);
+    else super.accept(visitor);
+  }
+
+}
